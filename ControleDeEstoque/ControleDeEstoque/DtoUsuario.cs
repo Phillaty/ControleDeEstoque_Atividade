55b2900a9f4f4ee2dc666acfb90fd3e89d93da98
GustavoVillasBoas <gustavovb33@gmail.com>
2022-04-01 23:24:45 -0300

    alt

---
+++ ControleDeEstoque/ControleDeEstoque/DtoUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeEstoque
+{
+    [Table("usuario", Schema = "public")]
+    internal class DtoUsuario
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string login { get; set; }
+        public string senha { get; set; }
+    }
+}
---
+++ ControleDeEstoque/ControleDeEstoque/context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeEstoque
+{
+    internal class context : DbContext
+    {
+        public context() : base("ba")
+        {
+            
+        }
+
+        public DbSet<DtoUsuario> usuarios { get; set; }
+    }
+}
