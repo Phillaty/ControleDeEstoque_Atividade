c6071cc235c51755bd4a6b0889a479a6c9225186
Gustavo <gustavovb33@gmail.com>
2022-04-09 21:08:54 -0300

    alt

---
+++ ControleDeEstoque/ControleDeEstoque/formUsuario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleDeEstoque
+{
+    public partial class formUsuario : Form
+    {
+        public formUsuario()
+        {
+            InitializeComponent();
+            CarregarGrid();
+        }
+
+        private void CarregarGrid()
+        {
+            Model model = new Model();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Model model = new Model();
+            DtoUsuario u = new DtoUsuario();
+            u.Name= nomeInput.Text; 
+            u.login= loginInput.Text;
+            u.senha= senhaInput.Text;
+
+            if(idInput.Text == string.Empty)
+            {
+                model.setUsuario(u);
+            }
+            else
+            {
+
+            }
+
+            label5.Text = "ACONTECE NADA";
+
+        }
+    }
+}
